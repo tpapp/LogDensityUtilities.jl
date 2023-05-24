a753ec77b1cfacea17cbc3ea6304485d29ad266b
Documenter.jl <documenter@juliadocs.github.io>
2023-05-24 12:41:21 +0000

    build based on 4601f8a

---
+++ previews/PR1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#LogDensityUtilities","page":"LogDensityUtilities","title":"LogDensityUtilities","text":"","category":"section"},{"location":"","page":"LogDensityUtilities","title":"LogDensityUtilities","text":"Documentation goes here.","category":"page"}]
+}
